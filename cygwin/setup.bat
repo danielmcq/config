0493a0dd40a436d3eef871749bb910480c216145
Daniel McQuiston <daniel.pro@dehugo.net>
2015-03-03 21:43:14 -0700

    Updated Cygwin setup.bat

--- cygwin/setup.bat
+++ cygwin/setup.bat
@@ -1,7 +1,14 @@
 REM See Cygwin FAQ http://www.cygwin.com/faq/faq.html#faq.setup.cli
 setup-x32_64.exe ^
---quite-mode ^
+--quiet-mode ^
+--no-desktop ^
+--site http://mirrors.xmission.com/cygwin/ ^
 --packages ^
 autossh,^
 curl,^
+git-svn,^
+git,^
+openssh,^
+subversion,^
+vim-common,^
 vim
